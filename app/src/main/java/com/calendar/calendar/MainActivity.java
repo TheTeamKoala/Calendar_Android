e7a0231d76c1618942e6279b5ad39ae7ae9f5cf8
Omer Mert Unal <omermertunal>
2018-07-08 17:00:20 +0500

    some delicious things

--- app/src/main/java/com/calendar/calendar/MainActivity.java
+++ app/src/main/java/com/calendar/calendar/MainActivity.java
@@ -1,5 +1,7 @@
 package com.calendar.calendar;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,10 +16,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
+
+    Button bfecha,bhora;
+    EditText efecha,ehora;
+    private int day,month,year,hour,minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        //for calend
+        bfecha =(Button) findViewById(R.id.bfecha);
+        bhora =(Button) findViewById(R.id.bhora);
+        efecha=(EditText)findViewById(R.id.efecha);
+        ehora = (EditText)findViewById(R.id.ehora);
+
+        bfecha.setOnClickListener(this);
+        bhora.setOnClickListener(this);
+
     }
 
     @Override
@@ -109,6 +131,44 @@ public class MainActivity extends AppCompatActivity
         intent = new Intent(MainActivity.this, Main2Activity.class);
 
         startActivity(intent);
+
+    }
+
+    @Override// for calend
+    public void onClick(View v) {
+
+        if(v == bfecha){
+            final Calendar c =Calendar.getInstance();
+            day = c.get(Calendar.DAY_OF_MONTH);
+            month = c.get(Calendar.MONTH);
+            year = c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    efecha.setText(dayOfMonth + "/" +(month+1)+"/"+year);
+
+                }
+            } ,day,month,year);
+            datePickerDialog.show();
+
+
+        }
+        if(v== bhora){
+
+            final Calendar c = Calendar.getInstance();
+            hour = c.get(Calendar.HOUR_OF_DAY);
+            minute = c.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    ehora.setText(hourOfDay+":" + minute);
+
+                }
+            },hour,minute,false);
+            timePickerDialog.show();
+        }
 
     }
 }
